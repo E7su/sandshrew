eb284c70ca0d681f8dea162c802dfe417a5300f6
Etsu <etsu4296@gmail.com>
2017-04-09 22:00:19 +0300

    Create CaesarCipher_for_university.py

---
+++ CaesarCipher_for_university.py
@@ -0,0 +1,55 @@
+# Шифр Цезаря
+
+# строка, которая будут зашифрована/расшифрована
+lang = input('Выберите язык исходного сообщения. (р - русский, а - английский)\n')
+message = input('Пожалуйста, введите сообщение:\n')
+# message = 'This is my secret message. WEBSERVER IS DOWN!!1'   # пример
+
+# ключ
+key = input('Выберите размер ключа:\n')
+# key = 13  # пример
+
+# режим работы программы (расшифровка/зашифровка)
+mode = 'encrypt'  # 'encrypt' или 'decrypt'
+
+# символы для шифрования
+
+if lang == 'р':
+    LETTERS = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+elif lang == 'а':
+    LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+else:
+    print('Введено неверное значение.')
+
+# хранит зашифрованную/дешифрованную форму сообщения
+translated = ''
+
+# приводит сообщение к верхнему регистру
+message = message.upper()
+
+# запускает код для посимвольного шифрования/дешифрования исходного сообщения
+for symbol in message:
+    if symbol in LETTERS:
+        # находит номер для данного символа (зашифрованного или расшифрованного)
+        num = LETTERS.find(symbol)  # берёт номер данного символа
+        if mode == 'encrypt':
+            num = num + key
+        elif mode == 'decrypt':
+            num = num - key
+
+        # handle the wrap-around if num is larger than the length of
+        # LETTERS or less than 0
+        if num >= len(LETTERS):
+            num = num - len(LETTERS)
+        elif num < 0:
+            num = num + len(LETTERS)
+
+        # добавляет зашифрованный/расшифрованный символ в конец
+        translated = translated + LETTERS[num]
+
+    else:
+        # добаляет символ без шифрования/дешифрования
+        translated = translated + symbol
+
+# вывод зашифрованного/расшифрованного сообщения
+print(translated)
